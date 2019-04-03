38adae0159bef6b11b036bfecba56d5a85aeca9f
abk1998 <47002728+abk1998@users.noreply.github.com>
2019-04-03 22:33:44 +0530

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,21 @@
+#Reading the data
+data <- data.table::fread(input = "household_power_consumption.txt", na.strings="?")
+
+#Preventing Scientific Notation
+data[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
+
+#Making a POSIXct date capable of being filtered and graphed by time of day
+data[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
+
+#Subsetting the Data according to given period
+data <- data[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
+
+#Creating the plot
+plot(data[, dateTime], data[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
+lines(data[, dateTime], data[, Sub_metering_2],col="red")
+lines(data[, dateTime], data[, Sub_metering_3],col="blue")
+legend("topright", col=c("black","red","blue"), c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  "),lty=c(1,1), lwd=c(1,1))
+
+#Saving the plot
+dev.copy(png, file = "plot3.png", height = 480, width = 480)
+dev.off()
